5d2f2420f463b3382e860f0fce28283d16678bfd
sschroeter <sinikka.schroeter@gmail.com>
2013-11-26 15:26:34 +0100

    make ideas from the sidebar draggable and droppable to the board, atm hard coded wishlist in BoardController

--- public/js/controllers/board.js
+++ public/js/controllers/board.js
@@ -9,8 +9,13 @@ angular.module('coolwall.board').controller('BoardController', ['$scope', 'Globa
 
     $scope.oDraggedModel = null;
 
+    $scope.wishlist =
+        [{'title': 'A', 'snippet':'Something', 'board_row':'', 'board_column':''},
+        {'title':'B','snippet':'Something else', 'board_row':'', 'board_column':''},
+        {'title':'C','snippet':'Whatever', 'board_row':'', 'board_column':''}];
+
     $scope.dropCallback = function(event, ui, column, row) {
-        var oModel = $scope.oDraggedModel;
+        var oModel = $scope.global.oDraggedModel;
 
         //assign new position
         oModel.board_row = row;
@@ -23,7 +28,7 @@ angular.module('coolwall.board').controller('BoardController', ['$scope', 'Globa
     };
 
     $scope.dragStartCallback = function(event, ui, item) {
-        $scope.oDraggedModel = item;
+        $scope.global.oDraggedModel = item;
     };
 
     $scope.updateIdeaTitle = function(idea) {
